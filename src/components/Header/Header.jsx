0df80e723bc5f93134e804db44b41826760c784c
romantymchuk <roman.t@aquimarketing.com>
2022-04-10 17:28:44 +0300

    Added logout, movielist, pagination, dynamic header profile/auth

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,61 +1,85 @@
 import React, { useState } from "react";
 import { Container, Navbar, Nav, Offcanvas } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-const Header = () => {
+const Header = (props) => {
 
     const [collapsed, setCollapsed] = useState(true);
 
     return <>
         <Navbar bg="light" variant="light" expand={collapsed}>
             {collapsed
-                ? <Container fluid>
-
-                    <Navbar.Brand as={Link} to="/">
-                        Home
-                    </Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link as={Link} to="/movies">
-                            Movies
-                        </Nav.Link >
-                        <Nav.Link as={Link} to="/profile">
-                            Profile
-                        </Nav.Link>
-                        <Nav.Link as={Link} to="/auth">
-                            Auth
-                        </Nav.Link>
-                    </Nav>
-
-                </Container>
-                : <Container fluid>
-                    <Navbar.Brand as={Link} to="/">
-                        Home
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="offcanvasNavbar" />
-                    <Navbar.Offcanvas
-                        id="offcanvasNavbar"
-                        aria-labelledby="offcanvasNavbarLabel"
-                    >
-                        <Offcanvas.Header closeButton>
-                            <Offcanvas.Title id="offcanvasNavbarLabel">Navigation</Offcanvas.Title>
-                        </Offcanvas.Header>
-                        <Offcanvas.Body>
-                            <Nav className="me-auto">
-                                <Nav.Link as={Link} to="/movies">
-                                    Movies
-                                </Nav.Link >
-                                <Nav.Link as={Link} to="/profile">
-                                    Profile
-                                </Nav.Link>
-                            </Nav>
-                        </Offcanvas.Body>
-                    </Navbar.Offcanvas>
-                </Container>
+                ? <NavCollapsed user={props.profile.user} />
+                : <NavExpanded user={props.profile.user} />
             }
-
         </Navbar >
-        <button onClick={() => setCollapsed(collapsed === true ? false : true)} ></button>
+        {/* <button onClick={() => setCollapsed(collapsed === true ? false : true)} ></button> */}
     </>
 }
 
-export default Header;
+const NavCollapsed = ({ user }) => {
+    return <Container fluid>
+        <Navbar.Brand as={Link} to="/">
+            Home
+        </Navbar.Brand>
+        <Nav className="me-auto">
+            <Nav.Link as={Link} to="/movies">
+                Movies
+            </Nav.Link >
+        </Nav>
+        <Navbar.Text className="justify-content-end">
+            {user
+                ? <Nav.Link as={Link} to="/profile">
+                    {user.name}
+                </Nav.Link>
+                : <Nav.Link as={Link} to="/auth">
+                    Auth
+                </Nav.Link>
+            }
+        </Navbar.Text>
+    </Container>
+}
+
+const NavExpanded = ({ user }) => {
+    return <Container fluid>
+        <Navbar.Brand as={Link} to="/">
+            Home
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="offcanvasNavbar" />
+        <Navbar.Offcanvas
+            id="offcanvasNavbar"
+            aria-labelledby="offcanvasNavbarLabel"
+        >
+            <Offcanvas.Header closeButton>
+                <Offcanvas.Title id="offcanvasNavbarLabel">Navigation</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+                <Nav className="me-auto">
+                    <Nav.Link as={Link} to="/movies">
+                        Movies
+                    </Nav.Link >
+                    <Nav.Link as={Link} to="/profile">
+                        Profile
+                    </Nav.Link>
+                </Nav>
+                <Navbar.Text>
+                    {user
+                        ? <Nav.Link as={Link} to="/profile">
+                            {user.name}
+                        </Nav.Link>
+                        : <Nav.Link as={Link} to="/auth">
+                            Auth
+                        </Nav.Link>
+                    }
+                </Navbar.Text>
+            </Offcanvas.Body>
+        </Navbar.Offcanvas>
+    </Container>
+}
+
+const mapStateToProps = (state, ownProps) => ({
+    ...state
+})
+
+export default connect(mapStateToProps, {})(Header);
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { Button } from "react-bootstrap";
+import { connect } from "react-redux";
+import { Navigate } from "react-router-dom";
+import { logoutUser } from "../../store/reducers/user.reducer";
 
-const Profile = () => {
+const Profile = (props) => {
+
+    const logout = () => {
+        localStorage.removeItem('auth_token');
+        props.logoutUser();
+    }
+
     return <div>
-        Profile
+        {!props.profile.user && <Navigate to="/auth" />}
+        <Button onClick={logout}>Logout</Button>
     </div>
 }
 
-export default Profile;
+const mapStateToProps = (state, ownProps) => ({
+    ...state
+})
+
+
+export default connect(mapStateToProps, { logoutUser })(Profile)
