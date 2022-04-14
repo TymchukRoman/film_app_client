d581b9e8a1bc747d3e4d3906c48c602e6820586a
romantymchuk <roman.t@aquimarketing.com>
2022-04-14 23:59:46 +0300

    Minor repository cleaning

--- src/App.js
+++ src/App.js
@@ -8,35 +8,35 @@ import { useEffect, useState } from 'react';
 
 const App = (props) => {
 
-  const [isUserLoaded, setIsUserLoaded] = useState(false);
+    const [isUserLoaded, setIsUserLoaded] = useState(false);
 
-  useEffect(() => {
-    const token = localStorage.getItem('auth_token');
-    console.log(token);
-    if (!token) {
-      setIsUserLoaded(true);
-    } else {
-      userMe(token).then((response) => {
-        if (response?.data?.user) {
-          props.setLoginedUser(response.data.user);
+    useEffect(() => {
+        const token = localStorage.getItem('auth_token');
+        console.log(token);
+        if (!token) {
+            setIsUserLoaded(true);
+        } else {
+            userMe(token).then((response) => {
+                if (response?.data?.user) {
+                    props.setLoginedUser(response.data.user);
+                }
+                setIsUserLoaded(true);
+            });
         }
-        setIsUserLoaded(true);
-      });
-    }
-  }, [])
+    }, [])
 
-  return <>
-    {
-      isUserLoaded
-        ? <RootRouter />
-        : <>Loading</>
-    }
-  </>
+    return <>
+        {
+            isUserLoaded
+                ? <RootRouter />
+                : <>Loading</>
+        }
+    </>
 }
 
 
 const mapStateToProps = (state, ownProps) => ({
-  ...state
+    ...state
 })
 
 
