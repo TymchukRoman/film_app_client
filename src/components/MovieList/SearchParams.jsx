9c0686f480e07b6e393f64896e9770d0b11b8aaa
romantymchuk <roman.t@aquimarketing.com>
2022-05-09 22:42:10 +0300

    Added params logic + minor fixes

--- src/components/MovieList/SearchParams.jsx
+++ src/components/MovieList/SearchParams.jsx
@@ -16,19 +16,25 @@ const SearchParams = (props) => {
             imdb: props.initialParams?.imdb || 0,
             genres: props.initialParams?.genres?.length
                 ? [...props.initialParams.genres.map((item) => ({ "value": item, "label": item }))] : [],
+            genresLogic: props.initialParams?.genresLogic || "$in",
             types: props.initialParams?.types?.length
                 ? [...props.initialParams.types.map((item) => ({ "value": item, "label": item }))] : [],
             withPoster: props.initialParams?.withPoster || false,
             actors: props.initialParams?.actors?.length ?
                 [...props.initialParams.actors.map((item) => ({ "value": item, "label": item }))] : [],
+            actorsLogic: props.initialParams?.actorsLogic || "$in",
             countries: props.initialParams?.countries?.length ?
                 [...props.initialParams.countries.map((item) => ({ "value": item, "label": item }))] : [],
+            countriesLogic: props.initialParams?.countriesLogic || "$in",
             languages: props.initialParams?.languages?.length ?
                 [...props.initialParams.languages.map((item) => ({ "value": item, "label": item }))] : [],
+            languagesLogic: props.initialParams?.languagesLogic || "$in",
             writers: props.initialParams?.writers?.length ?
                 [...props.initialParams.writers.map((item) => ({ "value": item, "label": item }))] : [],
+            writersLogic: props.initialParams?.writersLogic || "$in",
             directors: props.initialParams?.directors?.length ?
                 [...props.initialParams.directors.map((item) => ({ "value": item, "label": item }))] : [],
+            directorsLogic: props.initialParams?.directorsLogic || "$in",
             rates: props.initialParams?.rates?.length ?
                 [...props.initialParams.rates.map((item) => ({ "value": item, "label": item }))] : [],
         },
@@ -50,6 +56,12 @@ const SearchParams = (props) => {
                 writers: values.writers.map((item) => item.value),
                 directors: values.directors.map((item) => item.value),
                 rates: values.rates.map((item) => item.value),
+                genresLogic: values.genresLogic,
+                actorsLogic: values.actorsLogic,
+                countriesLogic: values.countriesLogic,
+                languagesLogic: values.languagesLogic,
+                writersLogic: values.writersLogic,
+                directorsLogic: values.directorsLogic,
             });
         },
     });
@@ -91,7 +103,7 @@ const SearchParams = (props) => {
     }
 
     return <div style={{ padding: "20px" }}>
-        <Accordion defaultActiveKey="0">
+        <Accordion>
             <Accordion.Item eventKey="0">
                 <Accordion.Header>
                     <div style={{ width: "100%" }}>
@@ -186,6 +198,9 @@ const SearchParams = (props) => {
                                             handler={handleSelectChanges('genres')}
                                             value={formik.values.genres}
                                             cat={"genres"}
+                                            logic={true}
+                                            logicHandler={handleSelectChanges("genresLogic")}
+                                            logicVal={formik.values.genresLogic}
                                         />
                                     </Form.Group>
                                 </Col>
@@ -215,6 +230,9 @@ const SearchParams = (props) => {
                                         handler={handleSelectChanges("actors")}
                                         value={formik.values.actors}
                                         cat={"actors"}
+                                        logic={true}
+                                        logicHandler={handleSelectChanges("actorsLogic")}
+                                        logicVal={formik.values.actorsLogic}
                                     />
                                 </Col>
                                 <Col>
@@ -222,6 +240,9 @@ const SearchParams = (props) => {
                                         handler={handleSelectChanges("countries")}
                                         value={formik.values.countries}
                                         cat={"countries"}
+                                        logic={true}
+                                        logicHandler={handleSelectChanges("countriesLogic")}
+                                        logicVal={formik.values.countriesLogic}
                                     />
                                 </Col>
                                 <Col>
@@ -229,6 +250,9 @@ const SearchParams = (props) => {
                                         handler={handleSelectChanges("languages")}
                                         value={formik.values.languages}
                                         cat={"languages"}
+                                        logic={true}
+                                        logicHandler={handleSelectChanges("languagesLogic")}
+                                        logicVal={formik.values.languagesLogic}
                                     />
                                 </Col>
                             </Row>
@@ -239,6 +263,9 @@ const SearchParams = (props) => {
                                         handler={handleSelectChanges("writers")}
                                         value={formik.values.writers}
                                         cat={"writers"}
+                                        logic={true}
+                                        logicHandler={handleSelectChanges("writersLogic")}
+                                        logicVal={formik.values.writersLogic}
                                     />
                                 </Col>
                                 <Col>
@@ -246,6 +273,9 @@ const SearchParams = (props) => {
                                         handler={handleSelectChanges("directors")}
                                         value={formik.values.directors}
                                         cat={"directors"}
+                                        logic={true}
+                                        logicHandler={handleSelectChanges("directorsLogic")}
+                                        logicVal={formik.values.directorsLogic}
                                     />
                                 </Col>
                                 <Col>
@@ -265,7 +295,7 @@ const SearchParams = (props) => {
     </div >
 }
 
-const AsyncMultiSelect = ({ handler, value, cat }) => {
+const AsyncMultiSelect = ({ handler, value, cat, logic, logicHandler, logicVal }) => {
 
     const [inputValue, setInputValue] = useState('');
     const [debounceTimeout, setDebounceTimeout] = useState(null);
@@ -297,35 +327,62 @@ const AsyncMultiSelect = ({ handler, value, cat }) => {
         return newValue;
     };
 
+    const handleLogicChange = (value) => {
+        logicHandler(value);
+    }
+
     return <>
         <Form.Label>{cat[0].toUpperCase() + cat.slice(1)}</Form.Label>
-        <AsyncSelect
-            cacheOptions
-            loadOptions={debounce(loadOptions)}
-            onInputChange={handleInputChange}
-            defaultOptions
-            onChange={handler}
-            defaultValue={value}
-            value={value}
-            isMulti
-            name={cat}
-            className="basic-multi-select"
-            classNamePrefix="select"
-        />
+        <Row>
+            <Col xs={(logic && value.length > 1) ? 8 : 12}>
+                <AsyncSelect
+                    cacheOptions
+                    loadOptions={debounce(loadOptions)}
+                    onInputChange={handleInputChange}
+                    defaultOptions
+                    onChange={handler}
+                    defaultValue={value}
+                    value={value}
+                    isMulti
+                    name={cat}
+                    className="basic-multi-select"
+                    classNamePrefix="select"
+                />
+            </Col>
+            {(logic && value.length > 1)
+                ? <Col xs={4}>
+                    <ButtonGroup className="btn-block">
+                        <Button
+                            variant={logicVal === "$in" ? "success" : "outline-success"}
+                            value={"$in"}
+                            onClick={() => { handleLogicChange("$in") }}>
+                            OR
+                        </Button>
+                        <Button
+                            variant={logicVal === "$all" ? "success" : "outline-success"}
+                            value={"$all"}
+                            onClick={() => { handleLogicChange("$all") }}>
+                            AND
+                        </Button>
+                    </ButtonGroup>
+                </Col>
+                : <></>
+            }
+        </Row>
+
     </>
 }
 
 const ParamsBadges = ({ values }) => {
 
-    console.log(values)
-
-    const generateForArray = (values) => {
+    const generateForArray = (values, logic = null) => {
         const badgeString = values.map(item => item.label).join(', ');
+        const logicString = logic && values.length > 1 ? (logic === "$in" ? "(in)" : "(all)") : "";
         return <Badge
             bg="info"
             style={{ marginLeft: "10px" }}
-            title={badgeString}>
-            {badgeString.length > 15 ? (badgeString.slice(0, 15) + "...") : badgeString}
+            title={badgeString + " " + logicString}>
+            {badgeString.length > 15 ? (badgeString.slice(0, 15) + "...") : badgeString} {logicString}
         </Badge>
     }
 
@@ -344,13 +401,13 @@ const ParamsBadges = ({ values }) => {
             <Badge bg="info" style={{ marginLeft: "10px" }}>IMDB: {values.imdb}+</Badge>}
         {values.withPoster > 0 &&
             <Badge bg="info" style={{ marginLeft: "10px" }}>With image</Badge>}
-        {values.genres?.length > 0 && generateForArray(values.genres)}
+        {values.genres?.length > 0 && generateForArray(values.genres, values.genresLogic)}
         {values.types?.length > 0 && generateForArray(values.types)}
-        {values.actors?.length > 0 && generateForArray(values.actors)}
-        {values.countries?.length > 0 && generateForArray(values.countries)}
-        {values.languages?.length > 0 && generateForArray(values.languages)}
-        {values.writers?.length > 0 && generateForArray(values.writers)}
-        {values.directors?.length > 0 && generateForArray(values.directors)}
+        {values.actors?.length > 0 && generateForArray(values.actors, values.actorsLogic)}
+        {values.countries?.length > 0 && generateForArray(values.countries, values.countriesLogic)}
+        {values.languages?.length > 0 && generateForArray(values.languages, values.languagesLogic)}
+        {values.writers?.length > 0 && generateForArray(values.writers, values.writersLogic)}
+        {values.directors?.length > 0 && generateForArray(values.directors, values.directorsLogic)}
         {values.rates?.length > 0 && generateForArray(values.rates)}
     </>
 }
